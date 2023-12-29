15fb2dda80bfec588c443fbdfc5f48049a21e717
K-ash-ish <kashishsondhiya@gmail.com>
2023-12-29 12:49:04 +0530

    file permission granted

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
   const ytDlpPath = path.join(__dirname, "resources", "yt-dlp.exe");
-
   console.log(os.hostname());
   console.log(os.platform());
   console.log(os.type());
@@ -42,48 +41,28 @@ app.get("/yt-dlp", (req, res) => {
   const { url } = req.query;
   const ytDlpPath = path.join(__dirname, "resources", "yt-dlp.exe");
   console.log("Inside YT_DLP ");
-  fs.stat(ytDlpPath, (err, stats) => {
-    if (err) {
-      console.error(`Error checking file permissions: ${err.message}`);
-    } else {
-      const mode = stats.mode & 0o777; // Extract the permission bits
-      console.log(
-        `File permissions for yt-dlp: ${mode.toString(8)} ${parseInt(
-          stats.mode.toString(8),
-          10
-        )}`
-      );
 
-      // Check if execute permission is granted
-      const executePermission = Boolean(mode & 0o111); // Check the execute bit
-      if (executePermission) {
-        console.log("Execute permission is granted.");
-      } else {
-        console.log("Execute permission is not granted.");
-      }
-    }
+  let command = `${ytDlpPath} -f bv*[ext=mp4]+ba/b -o - ${url}`;
+  console.log("path: ", ytDlpPath);
+  console.log("command: ", command);
+  const ls = spawn(command, [], { shell: true });
+
+  res.setHeader("Content-Type", "video/mp4");
+  res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
+
+  ls.stdout.on("data", (data) => {
+    console.log(data.toString());
+    res.write(data);
   });
-  // let command = `${ytDlpPath} -f bv*[ext=mp4]+ba/b -o - ${url}`;
-  // console.log("path: ", ytDlpPath);
-  // console.log("command: ", command);
-  // const ls = spawn(command, [], { shell: true });
 
-  // res.setHeader("Content-Type", "video/mp4");
-  // res.setHeader("Content-Disposition", `attachment; filename="video.mp4"`);
-
-  // ls.stdout.on("data", (data) => {
-  //   console.log(data.toString());
-  //   res.write(data);
-  // });
-
-  // ls.stderr.on("error", (data) => {
-  //   console.error("err: ", data.toString());
-  //   // res.end();
-  // });
-  // ls.on("close", (code) => {
-  //   res.end();
-  //   console.log(`child process exited with code ${code}`);
-  // });
+  ls.stderr.on("error", (data) => {
+    console.error("err: ", data.toString());
+    // res.end();
+  });
+  ls.on("close", (code) => {
+    res.end();
+    console.log(`child process exited with code ${code}`);
+  });
 });
 
 app.listen(PORT, () => {
