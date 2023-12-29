9c94714820e92a54d7f314c034339b8ccaabc32f
K-ash-ish <kashishsondhiya@gmail.com>
2023-12-29 12:39:57 +0530

    checking if permission is granted or not

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const { exec, spawn } = require("child_process");
 const os = require("os");
+const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
+  const ytDlpPath = path.join(__dirname, "resources", "yt-dlp.exe");
+
   console.log(os.hostname());
   console.log(os.platform());
   console.log(os.type());
   res.send("Hello world");
-  ``;
+
+  fs.chmodSync(ytDlpPath, 0o755);
+  fs.stat(ytDlpPath, (err, stats) => {
+    if (err) {
+      console.error(`Error checking file permissions: ${err.message}`);
+    } else {
+      const mode = stats.mode & 0o777; // Extract the permission bits
+      console.log(
+        `File permissions for yt-dlp: ${mode.toString(8)} ${parseInt(
+          stats.mode.toString(8),
+          10
+        )}`
+      );
+
+      // Check if execute permission is granted
+      const executePermission = Boolean(mode & 0o111); // Check the execute bit
+      if (executePermission) {
+        console.log("Execute permission is granted.");
+      } else {
+        console.log("Execute permission is not granted.");
+      }
+    }
+  });
 });
 
 app.get("/yt-dlp", (req, res) => {
